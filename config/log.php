79454128f9f517818e8648cd5705b36a17039742
hajime3333out <hotta@fastbid.asia>
2015-08-13 12:52:50 +0800

    config file

--- config/log.php
+++ config/log.php
@@ -4,6 +4,14 @@ if ( ! defined ('__APP__') ) {
     define('__APP__', __DIR__ . '/..');
 }
 
+if ( ! function_exists('get_launch_mode') ) {
+    function get_launch_mode() {
+        if ( isset($_SERVER['REMOTE_ADDR'])) return 'web';
+        return 'cmd';
+    }
+}
+
 return array(
-    'default' => __APP__ . "/app-" . date('Y-m-d') . ".log"
+    'default' => __APP__ . "/storage/logs/" . get_launch_mode() . "-" . date('Y-m-d') . ".log"
 );
+
